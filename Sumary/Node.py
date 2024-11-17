b5157a8b6819d45f114e2994fea8d5898b4227ba
KhangVBN <khangvu782004@gmail.com>
2024-11-18 00:17:16 +0700

    Add document of function

--- Sumary/Node.py
+++ Sumary/Node.py
@@ -1,10 +1,29 @@
 class Node:
     def __init__(self, concept):
+        """_summary_
+
+        Arguments:
+            concept -- Node content
+        """
+
         self.concept = concept.lower() 
         self.relationships = [] 
 
     def add_relationship(self, relation, target_node):
+        """_summary_
+
+        Arguments:
+            relation -- Relation of node
+            target_node -- Node have relationship with this node
+        """
+
         self.relationships.append((relation, target_node))
 
     def get_relationships(self):
+        """_summary_
+
+        Returns:
+            Node relationships
+        """
+
         return self.relationships
